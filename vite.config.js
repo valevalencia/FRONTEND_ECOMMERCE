478c18ff9b0c909332a7c4361901b82a5a088649
valevalencia <valentinavalenciacarvajal16@gmail.com>
2023-10-10 17:11:17 -0500

    build: changes packagejson
    changes packagejson

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // Configura la carpeta de salida como 'build'
+  }
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
